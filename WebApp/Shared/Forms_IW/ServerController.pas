7ff85dfa4350af6f1d7f3009ec9403048bc236e9
Yeimi Rodrigo Osorio Valladares <yeioso@hotmail.com>
2022-06-21 17:44:07 -0500

    Actualización de tiempos

--- WebApp/Shared/Forms_IW/ServerController.pas
+++ WebApp/Shared/Forms_IW/ServerController.pas
@@ -10,7 +10,7 @@ uses
   Generics.Collections;
 
 Const
-  Const_Version = 'Versión 2022.05.17 Rev 1.0 - Delphi Syndey 10.4 (Community) - Intraweb 15.2.54';
+  Const_Version = 'Versión 2022.05.29 Rev 1.0 - Delphi Syndey 10.4 (Community) - Intraweb 15.2.54';
   Const_Max_Record = 2000;
 
 Type
